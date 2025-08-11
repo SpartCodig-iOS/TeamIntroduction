b4ceda3c2e77e3ec1254af525d3ebbd40e1c39c6
홍석현 <ghdtjrgus333@naver.com>
2025-08-11 17:47:13 +0900

    feat. 팀원 라벨 ui 생성

--- TeamIntroduce/TeamIntroduce/Sources/Presnetaion/IntroduceMain/View/Components/TeamIntroductionRowView.swift
+++ TeamIntroduce/TeamIntroduce/Sources/Presnetaion/IntroduceMain/View/Components/TeamIntroductionRowView.swift
@@ -9,7 +9,30 @@ import SwiftUI
 
 struct TeamIntroductionRowView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            Circle()
+                .frame(width: 42, height: 42)
+            VStack(alignment: .leading, spacing: 4) {
+                Text("김철수")
+                    .pretendardFont(family: .SemiBold, size: 14)
+                    .foregroundStyle(.textPrimary)
+                
+                Text("iOS 개발자")
+                    .pretendardFont(family: .Regular, size: 12)
+                    .foregroundStyle(.textSecondary)
+                
+                MBTILabel(mbti: .enfj)
+            }
+            
+            Spacer()
+            
+            Image("ArrowRight")
+                .foregroundStyle(Color(hex: "717182"))
+        }
+        .padding(16)
+        .background(Color.staticWhite)
+        .clipShape(RoundedRectangle(cornerRadius: 12))
+        .shadow(radius: 1)
     }
 }
 
