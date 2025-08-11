f5349260d6922efc03aab3ee0f6df2a01ebb91be
Roy <shuwj81@daum.net>
2025-08-11 17:38:39 +0900

    ✨[feat]: 이미지 관련  추가

---
+++ TeamIntroduce/TeamIntroduce/Sources/DesignSytstem/Componet/ListRowComponet.swift
@@ -0,0 +1,18 @@
+//
+//  ListRowComponet.swift
+//  TeamIntroduce
+//
+//  Created by Wonji Suh  on 8/11/25.
+//
+
+import SwiftUI
+
+struct ListRowComponet: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ListRowComponet()
+}
