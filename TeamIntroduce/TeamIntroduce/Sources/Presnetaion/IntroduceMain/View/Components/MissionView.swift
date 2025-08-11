057447f81bd4c8d6d94403a0799750750fc5b1b1
홍석현 <ghdtjrgus333@naver.com>
2025-08-11 16:51:20 +0900

    [feat] mission view 컴포넌트 생성 및 이미지 추가

---
+++ TeamIntroduce/TeamIntroduce/Sources/Presnetaion/IntroduceMain/View/Components/MissionView.swift
@@ -0,0 +1,33 @@
+//
+//  MissionView.swift
+//  TeamIntroduce
+//
+//  Created by 홍석현 on 8/11/25.
+//
+
+import SwiftUI
+
+struct MissionView: View {
+    var body: some View {
+        VStack(alignment: .center, spacing: 12) {
+            Image("MissionLogo")
+                .renderingMode(.original)
+            
+            Text("우리의 미션")
+                .pretendardFont(family: .Bold, size: 14)
+                .foregroundStyle(.textPrimary)
+            
+            Text("혁신적인 기술로 사용자의 삶을 더 편리하고 풍요롭게 만드는 것")
+                .pretendardFont(family: .Regular, size: 12)
+                .foregroundStyle(.textSecondary)
+        }
+        .padding(16)
+        .background(Color(hex: "E9EBEF"))
+        .clipShape(RoundedRectangle(cornerRadius: 12))
+        .shadow(radius: 1, x: 2, y: 2)
+    }
+}
+
+#Preview {
+    MissionView()
+}
---
+++ TeamIntroduce/TeamIntroduce/Sources/Presnetaion/IntroduceMain/View/Components/TeamIntroductionRowView.swift
@@ -0,0 +1,18 @@
+//
+//  TeamIntroductionRowView.swift
+//  TeamIntroduce
+//
+//  Created by 홍석현 on 8/11/25.
+//
+
+import SwiftUI
+
+struct TeamIntroductionRowView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TeamIntroductionRowView()
+}
